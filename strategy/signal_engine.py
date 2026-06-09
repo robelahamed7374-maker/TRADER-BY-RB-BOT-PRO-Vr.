705632650025835f81e4526624ed02db489b8c56
robelahamed7374-maker <robelahamed7374@gmail.com>
2026-06-09 12:23:08 +0600

    Update signal_engine.py

--- strategy/signal_engine.py
+++ strategy/signal_engine.py
@@ -1,7 +1,33 @@
-class SmartMoneySignalEngine:
+from strategy.signal_engine import SmartMoneySignalEngine
 
-    def generate_signal(self):
 
-        return {
-            "signal": "WAIT"
-        }
+print("""
+======== SmartMoneyBot ========
+
+1 = OTC
+2 = REAL
+
+""")
+
+mode = input("Select Market: ")
+
+market = "REAL"
+
+if mode == "1":
+    market = "OTC"
+
+engine = SmartMoneySignalEngine()
+
+result = engine.generate_signal(
+    market=market
+)
+
+print("\n===== SIGNAL =====")
+
+print("Market:", result["market"])
+
+print("Signal:", result["signal"])
+
+print("BUY SCORE:", result["buy_score"])
+
+print("SELL SCORE:", result["sell_score"])
